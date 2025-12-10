57fe43b4fa57eedc01daa79fec15b0bf4ac04661
dan95 el guay <ddm19dev@gmail.com>
2025-12-10 19:01:10 +0100

    fix: origin

--- src/App.tsx
+++ src/App.tsx
@@ -360,7 +360,7 @@ function App() {
 
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
-      if (event.origin !== 'https://villazarcillo.vercel.app' /* && event.origin !== 'http://localhost:5174' */) {
+      if (event.origin !== 'hispania.thedm.es' /* && event.origin !== 'http://localhost:5174' */) {
         return
       }
 
