d349cba80f80e978f34f4631d7e9a737e1bbc932
dan95 el guay <ddm19dev@gmail.com>
2025-12-10 19:05:05 +0100

    fix: origin

--- src/App.tsx
+++ src/App.tsx
@@ -360,7 +360,7 @@ function App() {
 
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
-      if (event.origin !== 'hispania.thedm.es' /* && event.origin !== 'http://localhost:5174' */) {
+      if (event.origin !== 'https://hispania.thedm.es' /* && event.origin !== 'http://localhost:5174' */) {
         console.warn('Origen de mensaje no autorizado:', event.origin)
         return
       }
